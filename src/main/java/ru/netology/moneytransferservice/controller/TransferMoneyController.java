6db02019ffd72282a7f6923db2d851305ab724a1
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-12-13 14:19:11 +0300

    Refactor TransferMoneyController, application.properties

--- src/main/java/ru/netology/moneytransferservice/controller/TransferMoneyController.java
+++ src/main/java/ru/netology/moneytransferservice/controller/TransferMoneyController.java
@@ -1,6 +1,8 @@
 package ru.netology.moneytransferservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +15,7 @@ import ru.netology.moneytransferservice.model.TransferMoneyData;
 import ru.netology.moneytransferservice.service.TransferMoneyServiceInterface;
 
 @RequiredArgsConstructor
-@CrossOrigin(origins = "https://serp-ya.github.io/")
+@CrossOrigin(origins = "${myapp.allowed.origin}")
 @RestController
 public class TransferMoneyController implements TransferMoneyControllerInterface {
 
@@ -21,13 +23,15 @@ public class TransferMoneyController implements TransferMoneyControllerInterface
 
     @Override
     @PostMapping("/transfer")
-    public OperationStatus transfer(@Validated @RequestBody TransferMoneyData transferMoneyData) throws ErrorInputData {
-        return transferMoneyService.transfer(transferMoneyData);
+    public ResponseEntity<OperationStatus> transfer(@Validated @RequestBody TransferMoneyData transferMoneyData) throws ErrorInputData {
+        OperationStatus operationStatus = transferMoneyService.transfer(transferMoneyData);
+        return new ResponseEntity<>(operationStatus, HttpStatus.OK);
     }
 
     @Override
     @PostMapping("/confirmOperation")
-    public OperationStatus confirmOperation(@RequestBody ConfirmationData confirmationData) throws ErrorInputData {
-        return transferMoneyService.confirm(confirmationData);
+    public ResponseEntity<OperationStatus> confirmOperation(@RequestBody ConfirmationData confirmationData) throws ErrorInputData {
+        OperationStatus operationStatus = transferMoneyService.confirm(confirmationData);
+        return new ResponseEntity<>(operationStatus, HttpStatus.OK);
     }
 }
--- src/main/java/ru/netology/moneytransferservice/controller/TransferMoneyControllerInterface.java
+++ src/main/java/ru/netology/moneytransferservice/controller/TransferMoneyControllerInterface.java
@@ -1,12 +1,13 @@
 package ru.netology.moneytransferservice.controller;
 
+import org.springframework.http.ResponseEntity;
 import ru.netology.moneytransferservice.exception.ErrorInputData;
 import ru.netology.moneytransferservice.model.ConfirmationData;
 import ru.netology.moneytransferservice.model.OperationStatus;
 import ru.netology.moneytransferservice.model.TransferMoneyData;
 
 public interface TransferMoneyControllerInterface {
-    OperationStatus transfer(TransferMoneyData transferMoneyData) throws ErrorInputData;
+    ResponseEntity<OperationStatus> transfer(TransferMoneyData transferMoneyData) throws ErrorInputData;
 
-    OperationStatus confirmOperation(ConfirmationData confirmationData) throws ErrorInputData;
+    ResponseEntity<OperationStatus> confirmOperation(ConfirmationData confirmationData) throws ErrorInputData;
 }
